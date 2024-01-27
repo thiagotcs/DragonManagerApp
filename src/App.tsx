91341c3cb31b1cfdd7c3ee421d273c5dc5d6abe3
“thiagotcs” <thiagotcs.dev@gmail.com>
2024-01-27 09:06:36 -0300

    Refactor: Final adjustments and optimizations

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
+import { router } from './routes'
+import { GlobalStyles } from './styles/GlobalStyles'
+
+const queryCLient = new QueryClient()
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | Dragons APP" />
+        <Toaster richColors position="top-center" />
+        <QueryClientProvider client={queryCLient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </HelmetProvider>
+    </>
+  )
+}
